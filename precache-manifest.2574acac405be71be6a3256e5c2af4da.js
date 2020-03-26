01b6623f2785006569430994c6f3dc79b81516c7
meriem23 <esra.msallem@gmail.com>
2020-03-26 16:02:52 +0100

    Updates

---
+++ precache-manifest.2574acac405be71be6a3256e5c2af4da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf891641974c01e53135cc056bd619f9",
+    "url": "/movie-app-react-redux-router/index.html"
+  },
+  {
+    "revision": "c03f7033c609c092977e",
+    "url": "/movie-app-react-redux-router/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "1e75a00a65fe0f9b91bc",
+    "url": "/movie-app-react-redux-router/static/css/main.ec81a1fd.chunk.css"
+  },
+  {
+    "revision": "c03f7033c609c092977e",
+    "url": "/movie-app-react-redux-router/static/js/2.ed729c7f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie-app-react-redux-router/static/js/2.ed729c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e75a00a65fe0f9b91bc",
+    "url": "/movie-app-react-redux-router/static/js/main.f4cb9f5e.chunk.js"
+  },
+  {
+    "revision": "8e813480be6266233fc8",
+    "url": "/movie-app-react-redux-router/static/js/runtime-main.50bcd134.js"
+  }
+]);
